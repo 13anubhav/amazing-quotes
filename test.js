ab91897794c09cd11b01230d15a2fae0c624be75
anubhav <developer@noormertech.com>
2024-05-01 17:47:02 +0530

    Added Code

---
+++ test.js
@@ -0,0 +1,6 @@
+const getRandomQuote = require('amazing-quotes');
+
+// Example usage:
+const quote = getRandomQuote();
+console.log(quote.text); // Output: Random quote text
+console.log(quote.author); // Output: Random quote author
